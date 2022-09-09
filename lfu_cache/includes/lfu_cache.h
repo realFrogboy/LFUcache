8681a6700b8848e3121afda5bdaf7b0f83dd0c34
realFrogboy <avdeev.do@phystech.edu>
2022-09-09 20:47:42 +0500

    Bug fix

--- lfu_cache/includes/lfu_cache.h
+++ lfu_cache/includes/lfu_cache.h
@@ -10,11 +10,9 @@ template <typename T, typename FreqNode, typename KeyT = int>
 struct setNode_t {
     T data;
     KeyT key;
+    int freq;
 
-    using freqNodeIt = typename std::list<FreqNode>::iterator;
-    freqNodeIt freqNode;
-
-    setNode_t(T d, KeyT k, freqNodeIt iter) : data(d), key(k), freqNode(iter) {};
+    setNode_t(T d, KeyT k, int fr) : data(d), key(k), freq(fr) {};
 };
 
 template <typename T, typename KeyT = int>
@@ -37,22 +35,22 @@ struct cache_t {
     cache_t(size_t capacity, size_t sz) : cpty(capacity), size(sz) {}; // ctor
 
     int insert(const KeyT key) {
-        auto elem = hashTbl.find(key);
+        auto hashElem = hashTbl.find(key);
 
-        if (elem != hashTbl.end())      // key founded in cache
+        if (hashElem != hashTbl.end())      // key founded in cache
             return 1;
-                                        // key not founded
+                                            // key not founded
         size++;
         if (size > cpty) {
-            freqNode_t<int> lessFreq = freqLst.front();
-            auto node = lessFreq.lst.begin(); 
-            int key = node->key;
+            auto lessFreq = freqLst.begin();
+            auto setElem = lessFreq->lst.begin(); 
+            int key = setElem->key;
 
-            auto elem = hashTbl.find(key);
-            hashTbl.erase(elem);
-            lessFreq.lst.pop_front();
+            auto elemToDel = hashTbl.find(key);
+            hashTbl.erase(elemToDel);
+            lessFreq->lst.pop_front();
 
-            if (!lessFreq.lst.size()) 
+            if (!lessFreq->lst.size()) 
                 freqLst.pop_front();
 
             size--;
@@ -64,11 +62,11 @@ struct cache_t {
             freqLst.push_front(newNode);
         }
 
-        std::list<freqNode_t<int>>::iterator node = freqLst.begin();
-        setNode_t<int, freqNode_t<int>> newNode(key, key, node);    
+        std::list<freqNode_t<int>>::iterator freqNode = freqLst.begin();
+        setNode_t<int, freqNode_t<int>> newNode(key, key, 1);    
 
-        node->lst.push_front(newNode);
-        auto newNodeIt = node->lst.begin();
+        freqNode->lst.push_front(newNode);
+        auto newNodeIt = freqNode->lst.begin();
 
         hashTbl.insert({key, newNodeIt});
 
@@ -76,23 +74,31 @@ struct cache_t {
     }
 
     int access(const KeyT key) {
-        auto elem = hashTbl.find(key);
+        auto hashElem = hashTbl.find(key);
+        auto setElem = hashElem->second;
 
-        auto freq = elem->second->freqNode;
-        auto freqNext = std::next(freq, 1);
+        auto freq = setElem->freq;
+        auto freqElem = freqLst.begin();
+        for (auto iter = freqLst.begin(); iter != freqLst.end(); iter++) 
+            if (freq == iter->freq) {
+                freqElem = iter;
+                break;
+            }
+        
+        auto freqNext = std::next(freqElem, 1);
 
-        if ((freqNext == freqLst.end()) || (freq->freq != (freqNext->freq + 1))) {
-            freqNode_t<int> newNode(freq->freq + 1);    
+        if ((freqNext == freqLst.end()) || ((freqElem->freq + 1) != freqNext->freq)) {
+            freqNode_t<int> newNode(freqElem->freq + 1);    
             freqLst.insert(freqNext, newNode);
         }
 
-        freq->lst.erase(elem->second);
-        freqNext = std::next(freq, 1);
+        freqElem->lst.erase(setElem);
+        freqNext = std::next(freqElem, 1);
 
-        if (!freq->lst.size()) 
-            freqLst.erase(freq);
+        if (!freqElem->lst.size()) 
+            freqLst.erase(freqElem);
 
-        setNode_t<int, freqNode_t<int>> newNode(key, key, freqNext);    
+        setNode_t<int, freqNode_t<int>> newNode(key, key, freqNext->freq);    
 
         freqNext->lst.push_front(newNode);
 
