1f28e98427abf04fc78840ce2a2b2b21c34ef9da
realFrogboy <avdeev.do@phystech.edu>
2022-09-28 21:15:49 +0500

    Fix tests

--- ideal_cache/test/ideal_cache_unit_test.cpp
+++ ideal_cache/test/ideal_cache_unit_test.cpp
@@ -3,7 +3,7 @@
 
 TEST(CacheTest, test1) {
     std::vector<int> arr1 = {2, 2, 1, 3, 3, 4, 1, 5, 6, 2};
-    EXPECT_EQ(3, idealCache(10, 3, arr1));
+    EXPECT_EQ(4, idealCache(10, 3, arr1));
 }
  
 TEST(CacheTest, test2) {
@@ -13,7 +13,7 @@ TEST(CacheTest, test2) {
 
 TEST(CacheTest, test3) {
     std::vector<int> arr3 = {4, 7, 3, 6, 85, 34, 6, 8, 5, 3, 4, 7, 8, 6, 4, 4, 5, 6, 7, 8};
-    EXPECT_EQ(9, idealCache(20, 5, arr3));
+    EXPECT_EQ(11, idealCache(20, 5, arr3));
 }
 
 TEST(CacheTest, test4) {
@@ -23,12 +23,12 @@ TEST(CacheTest, test4) {
 
 TEST(CacheTest, test5) {
     std::vector<int> arr5 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    EXPECT_EQ(32, idealCache(33, 1, arr5));
+    EXPECT_EQ(33, idealCache(34, 1, arr5));
 }
 
 TEST(CacheTest, test6) {
     std::vector<int> arr6 = {2, 4, 2, 4, 6, 4, 6, 7, 6, 6, 7, 9, 6, 4, 3, 5, 7, 8, 6, 5, 4, 3, 4, 5, 7, 8, 8, 7, 6};
-    EXPECT_EQ(19, idealCache(29, 5, arr6));
+    EXPECT_EQ(20, idealCache(29, 5, arr6));
 }
 
 int main(int argc, char** argv) {
