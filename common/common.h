92032f33af9a4fe54da002e89d4ea6a1cff17dad
realFrogboy <avdeev.do@phystech.edu>
2022-10-02 04:30:26 +0500

    Add input for a costum type

--- common/common.h
+++ common/common.h
@@ -7,5 +7,19 @@
 #include <iostream>
 
 namespace common {
-std::vector<int> getData(size_t &n, size_t &cacheSz);
+
+template <typename KeyT>
+std::vector<KeyT> getData(size_t &n, size_t &cacheSz) {
+    std::cin >> cacheSz >> n;
+    assert(std::cin.good());
+
+    std::vector<KeyT> arr(n);
+
+    std::for_each(arr.begin(), arr.end(), [](KeyT& curr) {
+        std::cin >> curr;
+        assert(std::cin.good());
+    });
+    return arr;
 }
+
+} // common
